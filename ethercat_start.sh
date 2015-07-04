2995065b5e4064aecf9a092d4b9977268df6f32d
hex <hex@hex.com>
2015-07-04 10:46:01 +0800

    add ethercat.conf_backup files

--- ethercat_start.sh
+++ ethercat_start.sh
@@ -4,8 +4,8 @@ ip_address=${ip_strings#*HWaddr }
 ip_address=${ip_address:0:17}
 echo "ip address is: ${ip_address}"
 
-sed "s/\*\*:\*\*:\*\*:\*\*:\*\*:\*\*/$ip_address/g" /etc/ethercat.conf_backup > /etc/ethercat.conf
-sed "s/\*\*:\*\*:\*\*:\*\*:\*\*:\*\*/$ip_address/g" /etc/sysconfig/ethercat_backup > /etc/sysconfig/ethercat
+sed "s/\*\*:\*\*:\*\*:\*\*:\*\*:\*\*/$ip_address/g" /usr/Aris/resource/Aris_Control/ethercat.conf_backup > /etc/ethercat.conf
+sed "s/\*\*:\*\*:\*\*:\*\*:\*\*:\*\*/$ip_address/g" /usr/Aris/resource/Aris_Control/ethercat_backup > /etc/sysconfig/ethercat
 cp /etc/ethercat.conf /opt/etherlab/etc/ethercat.conf
 cp /etc/sysconfig/ethercat /opt/etherlab/etc/sysconfig/ethercat
 
