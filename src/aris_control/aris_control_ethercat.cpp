c4338fa7bcb9fa99e2fbe71bce290a896dd3862b
py0330 <py0330@sjtu.edu.cn>
2016-03-10 12:17:32 +0800

    sync

--- src/aris_control/aris_control_ethercat.cpp
+++ src/aris_control/aris_control_ethercat.cpp
@@ -440,8 +440,8 @@ namespace Aris
 #ifdef UNIX
 			rt_task_join(&imp->rt_task);
 #endif
-			ecrt_master_deactivate(imp->ec_master);
-			ecrt_release_master(imp->ec_master);
+			//ecrt_master_deactivate(imp->ec_master);
+			//ecrt_release_master(imp->ec_master);
 			imp->is_stopping_ = false;
 
 			imp->is_running_ = false;
