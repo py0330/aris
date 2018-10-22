2f00f7ef5d5241b3a5f828ad7675b4cf2b1438dd
py0330 <py0330@gmail.com>
2018-10-22 22:38:26 +0800

    make CMakeList clean

--- test/test_control_server/test_control_server.h
+++
@@ -1,6 +0,0 @@
-﻿#ifndef TEST_CONTROL_SERVER_
-#define TEST_CONTROL_SERVER_
-
-void test_control_server();
-
-#endif
--- test/test_core/test_core_xml.h
+++
@@ -1,6 +0,0 @@
-﻿#ifndef TEST_CORE_XML_H_
-#define TEST_CORE_XML_H_
-
-void test_core_xml();
-
-#endif
