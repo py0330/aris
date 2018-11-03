21095414ffd98618b19c4a9e53540f656def626a
py0330 <py0330@gmail.com>
2018-11-03 19:01:12 +0800

    move to xenomai 3

--- src/aris_control/aris_control_rt_timer.cpp
+++ src/aris_control/aris_control_rt_timer.cpp
@@ -69,7 +69,7 @@ namespace aris::control
 		std::any rt_task = RT_TASK();
 
 		// 为了使用返回值优化，这里必须判断，不能用三目运算符 //
-		if(rt_task_create(&std::any_cast<RT_TASK&>(rt_task), "realtime core", 0, 99, T_FPU | T_JOINABLE))
+		if(rt_task_create(&std::any_cast<RT_TASK&>(rt_task), "realtime core", 0, 99, T_JOINABLE))
 			rt_task = std::any();
 
 		return rt_task;
