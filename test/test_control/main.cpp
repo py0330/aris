674ab54b1427080ee50cccc856ad2f59ec99d1b2
py0330 <py0330@gmail.com>
2018-12-03 18:30:40 +0800

    make findpackage work for debug and release

--- test/test_control/main.cpp
+++ test/test_control/main.cpp
@@ -5,28 +5,11 @@
 #include "test_control_ethercat.h"
 #include "test_control_motion.h"
 
-
-void write_bit(char *data, int bit_size, char *pd, int offset, int bit_position)
-{
-
-
-
-}
-
-
-#include <bitset>
-
 int main(int argc, char *argv[])
 {
-
-	
-	
-	
-	
-	
-	//test_control_master_slave();
+	test_control_master_slave();
 	test_control_ethercat();
-	//test_control_motion();
+	test_control_motion();
 
     std::cout << "test_control finished, press any key to continue" << std::endl;
     std::cin.get();
