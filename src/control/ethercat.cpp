05cec0c4e28ad5efe24cd9f063b73526c889d2f5
py0330 <py0330@gmail.com>
2019-03-27 20:00:22 +0800

    make home correct

--- src/control/ethercat.cpp
+++ src/control/ethercat.cpp
@@ -164,12 +164,12 @@ namespace aris::control
 	{
 		std::size_t result_size;
 		std::uint32_t abort_code;
-		aris_ecrt_sdo_read(dynamic_cast<EthercatMaster&>(root()).ecHandle(), phyId(), index, subindex, reinterpret_cast<std::uint8_t*>(value), byte_size, &result_size, &abort_code);
+		aris_ecrt_sdo_read(ancestor<EthercatMaster>()->ecHandle(), phyId(), index, subindex, reinterpret_cast<std::uint8_t*>(value), byte_size, &result_size, &abort_code);
 	}
 	auto EthercatSlave::writeSdo(std::uint16_t index, std::uint8_t subindex, const void *value, aris::Size byte_size)->void
 	{
 		std::uint32_t abort_code;
-		aris_ecrt_sdo_write(dynamic_cast<EthercatMaster&>(root()).ecHandle(), phyId(), index, subindex, const_cast<std::uint8_t*>(reinterpret_cast<const std::uint8_t*>(value)), byte_size, &abort_code);
+		aris_ecrt_sdo_write(ancestor<EthercatMaster>()->ecHandle(), phyId(), index, subindex, const_cast<std::uint8_t*>(reinterpret_cast<const std::uint8_t*>(value)), byte_size, &abort_code);
 	}
 	EthercatSlave::~EthercatSlave() = default;
 	EthercatSlave::EthercatSlave(const std::string &name, std::uint16_t phy_id, std::uint32_t vid, std::uint32_t p_code, std::uint32_t r_num, std::uint32_t dc) :Slave(name, phy_id), imp_(new Imp)
