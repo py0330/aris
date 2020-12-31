e9eae43f759b4b94075fddec8bd3c0f87e3d7ebd
py0330 <py0330@gmail.com>
2020-12-31 15:30:01 +0800

    fix encoding of middle_ware.hpp

--- include/aris/server/middle_ware.hpp
+++ include/aris/server/middle_ware.hpp
@@ -1,4 +1,4 @@
-#ifndef ARIS_SERVER_MIDDLE_WARE_HPP_
+﻿#ifndef ARIS_SERVER_MIDDLE_WARE_HPP_
 #define ARIS_SERVER_MIDDLE_WARE_HPP_
 
 #include <string_view>
@@ -7,16 +7,14 @@
 #include <aris_lib_export.h>
 
 namespace aris::server {
-
-class ARIS_API MiddleWare {
-public:
-    MiddleWare() {}
-    virtual ~MiddleWare() {}
-
-    auto virtual executeCmd(std::string_view str, std::function<void(std::string)> send_ret)->int;     // 默认实现仅转发指令到控制器
-    auto virtual executeCmd(std::string cmd_str)->void;
-};
-
+	
+	class ARIS_API MiddleWare {
+	public:
+		MiddleWare() {}
+		virtual ~MiddleWare() {}
+		auto virtual executeCmd(std::string_view str, std::function<void(std::string)> send_ret)->int;     // 默认实现仅转发指令到控制器
+		auto virtual executeCmd(std::string cmd_str)->void;
+	};
 
 }   // namespace aris::server
 
