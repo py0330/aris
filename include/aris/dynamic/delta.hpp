0df2584cff604b9ae91a081826e07ab2bbbe9e0d
py0330 <py0330@gmail.com>
2021-10-22 01:17:35 +0800

    make complicated delta model

--- include/aris/dynamic/delta.hpp
+++ include/aris/dynamic/delta.hpp
@@ -4,7 +4,7 @@
 #include <array>
 #include <aris/dynamic/model_solver.hpp>
 
-namespace aris::dynamic{
+namespace aris::dynamic {
 	//---------------------------------------------------------------------------------------------
 	// 包含4个自由度。共4个杆件  link1~4
 	// 尺寸示意：
@@ -44,12 +44,15 @@ namespace aris::dynamic{
 	//                    o
 	//               支联3
 	//
-	
-	
+
+
 	/// @defgroup dynamic_model_group 动力学建模模块
 	/// @{
 	///
-	struct ARIS_API DeltaParam{
+
+	// 简化版的delta参数
+	// 适用于：三根支联对称分布
+	struct ARIS_API DeltaParam {
 		// DH PARAM //
 		double a{ 0.0 };
 		double b{ 0.0 };
@@ -65,31 +68,38 @@ namespace aris::dynamic{
 		double base2ref_pe[6]{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		std::string base2ref_pe_type;
 
-		// inertia vector, size must be 6
+		// inertia vector, size must be 4
 		std::vector<std::array<double, 10> > iv_vec;
 
-		// mot friction vector, size must be 6
+		// mot friction vector, size must be 4
 		std::vector<std::array<double, 3> > mot_frc_vec;
 	};
-	struct ARIS_API DeltaModel :public aris::dynamic::Model {
-	public:
-		virtual ~DeltaModel();
-		explicit DeltaModel(const DeltaParam &param);
-		DeltaModel(DeltaModel&&);
-		DeltaModel& operator=(DeltaModel&&);
 
-	private:
-		friend class DeltaInverseKinematicSolver;
-		friend class DeltaForwardKinematicSolver;
+	// 详细的delta参数
+	// 适用于每根支联尺寸不一样
+	struct ARIS_API DeltaFullParam {
+		// DH PARAM //
+		double a1{ 0.0 }, b1{ 0.0 }, c1{ 0.0 }, d1{ 0.0 }, e1{ 0.0 }, theta1{ 0.0 };
+		double a2{ 0.0 }, b2{ 0.0 }, c2{ 0.0 }, d2{ 0.0 }, e2{ 0.0 }, theta2{ aris::PI * 2 / 3 };
+		double a3{ 0.0 }, b3{ 0.0 }, c3{ 0.0 }, d3{ 0.0 }, e3{ 0.0 }, theta3{ -aris::PI * 2 / 3 };
+
+		// TOOL 0, by default is 321 type
+		double tool0_pe[6]{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		std::string tool0_pe_type;
+
+		// BASE wrt REF, by default is 321 type 
+		double base2ref_pe[6]{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		std::string base2ref_pe_type;
+
+		// inertia vector, size must be 4
+		std::vector<std::array<double, 10> > iv_vec;
+
+		// mot friction vector, size must be 4
+		std::vector<std::array<double, 3> > mot_frc_vec;
 	};
 
-
-
 	auto ARIS_API createModelDelta(const DeltaParam &param)->std::unique_ptr<aris::dynamic::Model>;
-
-
-
-
+	auto ARIS_API createModelDelta(const DeltaFullParam &param)->std::unique_ptr<aris::dynamic::Model>;
 
 	///
 	/// @}
