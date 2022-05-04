626e0572308aafcda8f6454591b1c64dcb7071ca
Yang Pan <py0330@gmail.com>
2022-05-04 17:45:15 +0800

    fix bug of PumaInverseSolver of which root

--- src/dynamic/puma.cpp
+++ src/dynamic/puma.cpp
@@ -604,8 +604,9 @@ namespace aris::dynamic{
 
 					double last_mp = imp_->motions[i]->mpInternal();
 					imp_->motions[i]->updP();
-					while (imp_->motions[i]->mpInternal() - last_mp > PI)imp_->motions[i]->setMpInternal(imp_->motions[i]->mpInternal() - 2 * PI);
-					while (imp_->motions[i]->mpInternal() - last_mp < -PI)imp_->motions[i]->setMpInternal(imp_->motions[i]->mpInternal() + 2 * PI);
+					imp_->motions[i]->setMpInternal(q[i]);
+					//while (imp_->motions[i]->mpInternal() - last_mp > PI)imp_->motions[i]->setMpInternal(imp_->motions[i]->mpInternal() - 2 * PI);
+					//while (imp_->motions[i]->mpInternal() - last_mp < -PI)imp_->motions[i]->setMpInternal(imp_->motions[i]->mpInternal() + 2 * PI);
 				}
 
 				return 0;
